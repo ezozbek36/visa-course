2e350b017b442ee48207180fd3e2bc715fb2ac3b
ezozbek36 <ezozbekrasulov789@gmail.com>
2025-12-07 23:15:59 +0500

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vituum from 'vituum'
 
 export default defineConfig(({ mode }) => ({
 	appType: 'mpa',
+	base: '/visa-course/dist',
 	plugins: [
 		...(mode === 'production' ? [vituum()] : []),
 		handlebars(),
